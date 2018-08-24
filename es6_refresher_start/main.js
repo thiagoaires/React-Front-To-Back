a8e791b578bb95b38bc2042ea11819f609959d07
Thiago Aires <thiago.aires@MAC-016.local>
2018-08-24 17:07:56 -0300

    firstCommit

---
+++ es6_refresher_start/main.js
@@ -0,0 +1,24 @@
+// CONST & LET
+
+// ARROW FUNCTIONS
+
+// FOREACH
+const frutas = ['Maças', 'Laranjas', 'Ovos']
+
+//frutas.forEach(fruta => console.log(fruta))
+// MAP
+
+const frutaSingular = frutas.map(fruta => fruta.slice(0,-1))
+console.log(frutaSingular)
+
+// FILTER
+
+// SPREAD
+
+// DESTRUCTURING
+
+// CLASSES
+
+// SUBCLASSES
+
+// MODULES
